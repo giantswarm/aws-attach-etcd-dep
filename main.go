9c5c48768a6f7ee3cbc8026d32a2fcf2511caabb
Vaclav Rozsypalek <vaclav@giantswarm.io>
2020-02-04 18:40:24 +0100

    fix flag name

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func mainError() error {
 	var err error
 
 	var f Flag
-	flag.Int64Var(&f.EniDeviceIndex, "eni-device-name", 1, "NIC Device index that will be used for attaching the ENI. Cannot be zeroas that is the default NCI that is already attached.")
+	flag.Int64Var(&f.EniDeviceIndex, "eni-device-index", 1, "NIC Device index that will be used for attaching the ENI. Cannot be zeroas that is the default NCI that is already attached.")
 	flag.BoolVar(&f.EniForceDetach, "eni-force-detach", false, "If set to true, app will use force-detach if the ENI cannot be detached by normal detach operation..")
 	flag.StringVar(&f.EniTagKey, "eni-tag-key", "aws-attach-by-id", "Tag key that will be used to found the requested ENI in AWS API.")
 	flag.StringVar(&f.EniTagValue, "eni-tag-value", "test", "Tag value that will be used to found the requested ENI in AWS API, this tag should identify one unique ENI.")
