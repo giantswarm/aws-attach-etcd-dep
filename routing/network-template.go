01f374029da88b8383dd0fc6a1aeaa28f4d2be9a
calvix <vaclav@giantswarm.io>
2020-05-12 15:26:18 +0200

    'try scope2'

--- routing/network-template.go
+++ routing/network-template.go
@@ -3,8 +3,10 @@ package routing
 const networkRoutingTemplate = `# ensure that traffic arriving on eth1 leaves again from eth1 to prevent asymetric routing
 [Match]
 Name=eth1
-[Network]
+
+[Address]
 Address={{.ENIAddress}}/{{.ENISubnetSize}}
+Scope=2
 
 [RoutingPolicyRule]
 Table=2
@@ -13,9 +15,4 @@ From={{.ENIAddress}}/32
 [Route]
 Destination=0.0.0.0/0
 Gateway={{.ENIGateway}}
-Table=2
-
-[Route]
-Destination={{.ENISubnet}}
-Table=2
-Scope=link`
+Table=2`
